327b6d83a9468018f5a61ad94b9c9125415529cf
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-10-28 12:17:51 -0700

    updated the configure file in the code to adjust the database in nosql

--- server/config/cleanDB.js
+++ server/config/cleanDB.js
@@ -2,29 +2,16 @@ const mongoose = require('mongoose');
 const models = require('../models');
 const db = require('./connection.js');
 
-module.exports = async () => {
+module.exports = async (modelName, collectionName) => {
     try {
-        // Specify the database name and collection names
-        const databaseName = 'npm-run-start';
-        const collectionNames = ['orders', 'users', 'products'];
-
-        // Use the specified database name
-        const dbConnection = mongoose.connection.useDb(databaseName);
-
-        // Loop through each collection name and drop it
-        for (const collectionName of collectionNames) {
-            const collection = dbConnection.collection(collectionName);
-            const collectionInfo = await collection.listIndexes().toArray();
-            
-            if (collectionInfo.length) {
-                // The collection exists, you can drop it
-                await collection.drop();
-                console.log(`Collection ${collectionName} in database ${databaseName} dropped.`);
-            } else {
-                console.log(`Collection ${collectionName} in database ${databaseName} not found.`);
-            }
-        }
+      let modelExists = await models[modelName].db.db.listCollections({
+        name: collectionName
+      }).toArray()
+  
+      if (modelExists.length) {
+        await db.dropCollection(collectionName);
+      }
     } catch (err) {
-        throw err;
+      throw err;
     }
-}
+  }
